a4df8733564953ab2f2cf283dafa1d54ea523e66
GoranTisma <goran89tisma@yahoo.com>
2018-12-07 15:33:46 +0100

    a

--- cypress/integration/examples/example.js
+++ cypress/integration/examples/example.js
@@ -1,15 +1,10 @@
 describe('Example', function () {
-    beforeEach(function () {
-<<<<<<< HEAD
-        cy.visit('/')
-      })
-=======
-        cy.visit('localhost:8080')
-      })
+  beforeEach(function () {
+      cy.visit('localhost:8080')
+    })
 
     it('get', function () {
-        cy
-          .get1();
-    });
-    });
->>>>>>> 46ccf9950d948302f80f108796857021e62668cf
+      cy
+        .get1();
+  })
+});
